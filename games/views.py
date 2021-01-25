f59ffaf4e8f170acc0b2e2fbc1bf5d0720641234
BonnieMcb <bonniemcb@gmail.com>
2021-01-25 19:23:01 +0000

    Add views and urls for games app.

--- games/views.py
+++ games/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_games(request):
+
+    games = Product.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'games/games.html', context)
