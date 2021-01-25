57d674012c084aeb4ae3457efec86f11a881089b
BonnieMcb <bonniemcb@gmail.com>
2021-01-25 14:40:32 +0000

    Edit admin display.

--- games/admin.py
+++ games/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from .models import Product, Category, Mechanic
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Mechanic)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'names',
+        'price',
+    )
+
+    ordering = ('rank',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class MechanicAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Mechanic, MechanicAdmin)
