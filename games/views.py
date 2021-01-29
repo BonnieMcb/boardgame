9696a03ef8f70bb1f85141ec156f7d79b4abf67c
BonnieMcb <bonniemcb@gmail.com>
2021-01-29 14:49:05 +0000

    Add nav bar that only displays on game page AND Add html for filtering.

--- games/views.py
+++ games/views.py
@@ -1,17 +1,24 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Product
+from .models import Mechanic
+from .models import Category
 
 # Create your views here.
 
 
 def all_games(request):
 
-    games = Product.objects.all().order_by('rank')  # Ignore the
+    games = Product.objects.all().order_by('rank')  # Ignore lint error
 
     paginator = Paginator(games, 48)  # Show 48 games per page
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'games/games.html', {'page_obj': page_obj})
+    mechanics = Mechanic.objects.all()
+    categories = Category.objects.all()
+
+    return render(request, 'games/games.html', {
+        'page_obj': page_obj, 'mechanics': mechanics,
+        'categories': categories})
