f2ae1ea50d469e935bec2291e494c6b2cf61035a
BonnieMcb <bonniemcb@gmail.com>
2021-02-10 12:52:18 +0000

    Add new app: bag, for viewing items added to bag.

--- boardgame/settings.py
+++ boardgame/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'games',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- boardgame/urls.py
+++ boardgame/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('games/', include('games.urls'))
+    path('games/', include('games.urls')),
+    path('bag/', include('bag.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
