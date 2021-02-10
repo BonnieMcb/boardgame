05620314b7be32974f3b1cf5f3e1a2cb1f4efa16
BonnieMcb <bonniemcb@gmail.com>
2021-02-10 10:41:50 +0000

    Fix sorting functionality so that category and mechanic queries can be sorted.

--- games/views.py
+++ games/views.py
@@ -20,18 +20,6 @@ def all_games(request):
     direction = None
 
     if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                games = games.annotate(lower_name=Lower('name'))
-
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            games = games.order_by(sortkey)
 
         if 'category' in request.GET:
             category = request.GET['category']
@@ -52,27 +40,49 @@ def all_games(request):
             queries = Q(names__icontains=query)
             games = games.filter(queries)
 
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                games = games.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            games = games.order_by(sortkey)
+
     paginator = Paginator(games, 48)  # Show 48 games per page
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    # add product categories for printing in template
+    # To pass through the cats and mechs for each game
     for product in page_obj:
         product.categories = product.category_id.all()
         product.mechanics = product.mechanic_id.all()
 
+    # To create the sidebar links
     mechanics = Mechanic.objects.all()
     categories = Category.objects.all()
 
     current_sorting = f'{sort}_{direction}'
+
+    # To create the sorting URLs
+    # With help from: https://stackoverflow.com/questions/11280948/best-way-to-get-query-string-from-a-url-in-python
+    # and https://stackoverflow.com/questions/4591525/is-it-possible-to-pass-query-parameters-via-djangos-url-template-tag
+    get_params = ''
+    url_params = request.GET.urlencode()
+    if url_params:
+        get_params = url_params + '&'
 
     context = {
         'page_obj': page_obj,
         'mechanics': mechanics,
         'categories': categories,
         'search_term': query,
-        'current_categories': categories,
+        'get_params': get_params,
         'current_sorting': current_sorting,
     }
 
@@ -84,9 +94,11 @@ def game_detail(request, game_id):
 
     game = get_object_or_404(Product, pk=game_id)
 
+    # To pass through the cats and mechs for each game
     game.categories = game.category_id.all()
     game.mechanics = game.mechanic_id.all()
 
+    # To create the sidebar links
     mechanics = Mechanic.objects.all()
     categories = Category.objects.all()
 
