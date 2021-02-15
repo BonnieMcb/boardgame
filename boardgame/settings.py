f18dc39f24ea4de7a3e272516c6ba86d4a0aa9b0
BonnieMcb <bonniemcb@gmail.com>
2021-02-15 09:37:00 +0000

    Add basic Stripe checkout functionality.

--- boardgame/settings.py
+++ boardgame/settings.py
@@ -167,3 +167,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
