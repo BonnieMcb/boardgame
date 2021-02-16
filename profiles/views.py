8062ca142b7d28b4309d051dd641912586a06879
BonnieMcb <bonniemcb@gmail.com>
2021-02-16 13:57:06 +0000

    Add profiles app.

---
+++ profiles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def profile(request):
+    # To display the user's profile.
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
