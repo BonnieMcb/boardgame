ad6dcf7f9e7b4ef6f94949d5589ee24b9ea815b5
BonnieMcb <bonniemcb@gmail.com>
2021-02-21 15:42:02 +0000

    Add functionality on bag page to add/remove items.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 MEMBERSHIP_ID_MIN = 5000
 
@@ -40,3 +40,35 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+# To adjust the quantity of a specific product in the bag to specified amount
+def adjust_bag(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    item_id_int = int(item_id)
+    if item_id_int >= MEMBERSHIP_ID_MIN:
+        handle_membership(request, bag)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
