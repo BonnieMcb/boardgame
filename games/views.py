2802663b453f07d309001ceb8d1f13856e642cef
BonnieMcb <bonniemcb@gmail.com>
2021-02-22 10:39:37 +0000

    Finalise styling on games page, including filtering and nav bar

--- games/views.py
+++ games/views.py
@@ -13,6 +13,68 @@ from bag.contexts import get_discount
 # Create your views here.
 
 
+def handle_url_parameters(request):
+
+    # To create the sorting URLs
+    # With help from: https://stackoverflow.com/questions/11280948/best-way-to-get-query-string-from-a-url-in-python
+    # and https://stackoverflow.com/questions/4591525/is-it-possible-to-pass-query-parameters-via-djangos-url-template-tag
+    base = request.GET.urlencode() + '&'
+    get_params = {
+        'no_page': base,
+        'no_sort': base,
+        'no_cat': base,
+        'no_mech': base,
+        'no_q': base
+    }
+
+    url_params = request.GET.urlencode()
+    if url_params:
+        # remove params as needed
+        has_page = 'page' in request.GET
+        has_sort = 'sort' in request.GET
+        has_cat = 'category' in request.GET
+        has_mech = 'mechanic' in request.GET
+        has_q = 'q' in request.GET
+
+        if has_page:
+            copy = request.GET.copy()
+            copy.pop('page')
+            get_params['no_page'] = copy.urlencode() + '&'
+
+        if has_sort or has_page:
+            copy = request.GET.copy()
+            if has_sort:
+                copy.pop('sort')
+                copy.pop('direction')
+            if has_page:
+                copy.pop('page')
+
+            get_params['no_sort'] = copy.urlencode()
+
+        if has_cat:
+            copy = request.GET.copy()
+            copy.pop('category')
+            if has_page:
+                copy.pop('page')
+            get_params['no_cat'] = copy.urlencode()
+
+        if has_mech:
+            copy = request.GET.copy()
+            copy.pop('mechanic')
+            if has_page:
+                copy.pop('page')
+            get_params['no_mech'] = copy.urlencode()
+
+        if has_q:
+            copy = request.GET.copy()
+            copy.pop('q')
+            if has_page:
+                copy.pop('page')
+            get_params['no_q'] = copy.urlencode()
+
+    return get_params
+
+
 def all_games(request):
     """ A view to show all products """
 
@@ -21,6 +83,9 @@ def all_games(request):
     categories = None
     sort = None
     direction = None
+
+    category = None
+    mechanic = None
 
     if request.GET:
 
@@ -62,7 +127,7 @@ def all_games(request):
         for game in games:
             game.discounted_price = round(discount * float(game.price), 2)
 
-    paginator = Paginator(games, 48)  # Show 48 games per page
+    paginator = Paginator(games, 60)  # Show 60 games per page
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -76,15 +141,22 @@ def all_games(request):
     mechanics = Mechanic.objects.all()
     categories = Category.objects.all()
 
-    current_sorting = f'{sort}_{direction}'
+    # handle URL parameters
+    get_params = handle_url_parameters(request)
 
-    # To create the sorting URLs
-    # With help from: https://stackoverflow.com/questions/11280948/best-way-to-get-query-string-from-a-url-in-python
-    # and https://stackoverflow.com/questions/4591525/is-it-possible-to-pass-query-parameters-via-djangos-url-template-tag
-    get_params = ''
-    url_params = request.GET.urlencode()
-    if url_params:
-        get_params = url_params + '&'
+    # show current filter/sort
+    current_filter_cat = None
+    current_filter_mech = None
+    if category:
+        cat = Category.objects.get(name=category)
+        current_filter_cat = cat.friendly_name
+    elif mechanic:
+        mech = Mechanic.objects.get(name=mechanic)
+        current_filter_mech = mech.friendly_name
+
+    current_sorting = None
+    if sort:
+        current_sorting = f'{sort} / {direction}'
 
     context = {
         'page_obj': page_obj,
@@ -92,7 +164,10 @@ def all_games(request):
         'categories': categories,
         'search_term': query,
         'get_params': get_params,
-        'current_sorting': current_sorting
+        'current_sorting': current_sorting,
+        'current_filter_cat': current_filter_cat,
+        'current_filter_mech': current_filter_mech,
+        'current_query': query
     }
 
     return render(request, 'games/games.html', context)
