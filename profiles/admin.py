ef60a131f8bc0b6892787dc8a6940b56cbb0aa2e
BonnieMcb <bonniemcb@gmail.com>
2021-02-23 13:56:26 +0000

    Add admin view for UserProfiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county'
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -11,17 +11,16 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # TODO: Add the ability to save defaults
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
