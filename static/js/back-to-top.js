8936dc258bdd6641f52d307c2aca9e3dcae07dd6
BonnieMcb <bonniemcb@gmail.com>
2021-02-27 12:21:35 +0000

    Add back to top button to events page, and rename js file

---
+++ static/js/back-to-top.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // Back to top button (referenced in Readme.md)
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
