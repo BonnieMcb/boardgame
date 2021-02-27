a93603a1d47ddee417a73546b002a20c619e33b9
BonnieMcb <bonniemcb@gmail.com>
2021-02-27 11:49:28 +0000

    Change Stripe currency to 'eur' to match rest of website prices.

--- boardgame/settings.py
+++ boardgame/settings.py
@@ -201,7 +201,7 @@ if 'USE_AWS' in os.environ:
 
 
 # Stripe
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
